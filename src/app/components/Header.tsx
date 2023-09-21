8609b174b0a28330026b535f6d3ba97dd44c258d
mumo-esther <mumoesther2021@gmail.com>
2023-09-21 22:31:33 +0300

    Update header compoent

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,23 @@
+function Header() {
+  return (
+    <header>
+    <div className="bg-gray-200 py-4 text-black-20 text-center">
+        <h1 className="text-2xl font-semibold">GitHub Issue Manager</h1>
+    </div>
+    <div className="bg-white p-6 rounded-lg shadow-md text-center">
+  <h2 className="text-xl font-semibold mb-2">Label issues for new Contributors</h2>
+  <p className="text-gray-600">
+    Now, Github will help potential first-time contributors{' '}
+    <a href="#" className="text-blue-500 hover:underline">
+      discover issues </a>
+       labelled with
+      <button className="bg-blue-500 hover:bg-blue-600 text-white px-1 py-1 ml-2 rounded-md">
+        good first issue
+      </button>
+  </p>
+</div>
+    </header>
+  )
+}
+
+export default Header
