7453646c4b5c8064f650759f0f89e5f8a1c2874a
mumo-esther <mumoesther2021@gmail.com>
2023-09-21 22:34:16 +0300

    Add NewIssueButton

---
+++ src/app/components/NewIssueButton.tsx
@@ -0,0 +1,15 @@
+
+import Link  from "next/link";
+
+function NewIssueButton() {
+  return (
+   <Link href='/NewIssue'>
+    <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md ml-3">
+   New Issue
+ </button> 
+   </Link>
+     
+  );
+}
+
+export default NewIssueButton;
