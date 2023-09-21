c6e3c009f806c9194b150b0adab01b15616a1540
mumo-esther <mumoesther2021@gmail.com>
2023-09-21 22:30:07 +0300

    Add interface typings

---
+++ src/app/interfaces/issueTypes.tsx
@@ -0,0 +1,37 @@
+export interface Issue {
+    id: number;
+    title: string;
+    created_at: Date;
+    user: User;
+    number: number;
+    comments: string;
+    state: string;
+    body: string;
+  }
+  
+  interface User {
+    login: string;
+    avatar_url: string;
+  }
+  
+ export interface IssueDetailsProps {
+    issue: Issue;
+    onClose: () => void;
+  }
+
+  export interface IssueListProps {
+    filter: string; 
+  }
+
+  export interface SearchBarProps {
+    onSearch: (query: string) => void;
+  }
+
+ export  interface IssueHeaderProps {
+    onFilterChange: (filter: string) => void;
+  }
+
+  export interface NavbarProps {
+    onFilterChange: (filter: string) => void;
+    onSearch: (query: string) => void;
+  }
