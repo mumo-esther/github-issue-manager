07867c048e581401662a3feb4b26a19b6419e43d
mumo-esther <mumoesther2021@gmail.com>
2023-09-22 21:42:23 +0300

    Update Navbar props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,28 @@
 import Header from "@/app/components/Header";
 import Navbar from "@/app/components/Navbar";
 import IssueList from "./components/IssueList";
+import { SearchProvider } from "./context/SearchContext";
+import { FilterProvider } from "./context/FilterContext";
 
 export default function Home() {
+  const handleFilterChange = (filter: any) => {
+    console.log("Filter changed:", filter);
+  };
+
+  const handleSearch = (query: any) => {
+    console.log("Search query:", query);
+  };
   return (
+    <FilterProvider>
+    <SearchProvider >
     <div>
       <Header />
       <main className="container mx-auto p-4">
-        < Navbar 
-        onFilterChange={function (filter: string): void {
-          throw new Error("Function not implemented.");
-        } } onSearch={function (query: string): void {
-          throw new Error("Function not implemented.");
-        }  } />
+      <Navbar onFilterChange={handleFilterChange} onSearch={handleSearch} />
         <IssueList filter={""} />
       </main>
     </div>
+    </SearchProvider>
+    </FilterProvider>
   )
 }
