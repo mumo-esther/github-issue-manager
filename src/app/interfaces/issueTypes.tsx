be53029a948e5f790145e32303132c59a9453910
mumo-esther <mumoesther2021@gmail.com>
2023-09-24 18:16:12 +0300

    Add label and milestone interfaces

--- src/app/interfaces/issueTypes.tsx
+++ src/app/interfaces/issueTypes.tsx
@@ -22,6 +22,7 @@ export interface Issue {
 
   export interface IssueListProps {
     filter: string; 
+    issues: Issue[];
   }
 
   export interface SearchBarProps {
@@ -39,4 +40,51 @@ export interface Issue {
 
   export interface Comment {
     commentId : number
+  }
+
+  export interface Assignee {
+    id: number;
+    login: string;
+  }
+
+ export interface Label {
+    id: number;
+    name: string;
+  }
+  
+  export interface Milestone {
+    id: number;
+    title: string;
+  }
+
+  export interface FilterContextType {
+    filter: string;
+    setFilter: React.Dispatch<React.SetStateAction<string>>;
+    selectedMilestone: string;
+    setSelectedMilestone: React.Dispatch<React.SetStateAction<string>>;
+    selectedLabel: string;
+    setSelectedLabel: React.Dispatch<React.SetStateAction<string>>;
+    selectedAssignee: string;
+    setSelectedAssignee: React.Dispatch<React.SetStateAction<string>>;
+  }
+
+  export interface Milestone {
+    id: number;
+    title: string;
+    updated_at: Date;
+    user: User;
+    number: number;
+    comments: string;
+    state: string;
+    description: string;
+    closed_issues: number;
+    open_issues: number;
+  }
+
+  export interface Label {
+    id: number;
+    name: string;
+    color: string,
+    description: string;
+    default: boolean;
   }
