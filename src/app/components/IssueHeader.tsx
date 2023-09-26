a13217cb906666d0227fdd646d911d7dd1863e5d
mumo-esther <mumoesther2021@gmail.com>
2023-09-26 11:30:02 +0300

    Add open/closed issues count

--- src/app/components/IssueHeader.tsx
+++ src/app/components/IssueHeader.tsx
@@ -3,43 +3,37 @@ import { useFilterContext } from "../context/FilterContext";
 import Selects from "./Selects";
 import React, { useState, useEffect } from "react";
 
+interface Issue {
+  state: "open" | "closed"; // Add other properties as needed
+}
+
 function IssueHeader() {
-  const [openIssuesCount, setOpenIssuesCount] = useState(0);
-  const [closedIssuesCount, setClosedIssuesCount] = useState(0);
+  const [openIssuesCount, setOpenIssuesCount] = useState<number>(0);
+  const [closedIssuesCount, setClosedIssuesCount] = useState<number>(0);
   const { filter, setFilter } = useFilterContext();
 
   useEffect(() => {
+    // Fetch open issues
     fetchOpenIssues()
-    .then((response) => {
-      console.log(response); // Log the response object
-      if (!response.ok) {
-        throw new Error(`Open issues request failed with status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data); // Log the parsed data
-      setOpenIssuesCount(data.length);
-    })
-    .catch((error) => {
-      console.error("Error:", error.message);
-    });
+      .then((data: Issue[]) => {
+        const openCount = data.filter((issue) => issue.state === "open").length;
+        setOpenIssuesCount(openCount);
+      })
+      .catch((error: Error) => {
+        console.error("Error fetching open issues:", error);
+      });
 
+    // Fetch closed issues
     fetchClosedIssues()
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Closed issues request failed with status: ${response.status}`);
-        }
-        return response.json();
+      .then((data: Issue[]) => {
+        const closedCount = data.filter((issue) => issue.state === "closed").length;
+        setClosedIssuesCount(closedCount);
       })
-      .then((data) => {
-        setClosedIssuesCount(data.length);
-      })
-      .catch((error) => {
+      .catch((error: Error) => {
         console.error("Error fetching closed issues:", error);
       });
   }, []);
-  
+
   return (
     <div className="flex flex-col lg:flex-row justify-between items-center p-4 bg-gray-200">
       <div className="flex flex-wrap items-center space-y-2 lg:space-y-0 lg:space-x-4 mb-2 lg:mb-0">
@@ -50,7 +44,6 @@ function IssueHeader() {
 
         <div className="w-5 h-5 border border-black rounded-full flex items-center justify-center mb-4">
           <div className="w-1 h-1 bg-black rounded-full"></div>
-
         </div>
         <span className="text-gray-700 mr-4 lg:mr-0">Open: {openIssuesCount}</span>
         <span className="text-gray-700">Closed: {closedIssuesCount}</span>
