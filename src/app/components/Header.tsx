3abe25882d6109d645d4625c2810e71f68243f06
mumo-esther <mumoesther2021@gmail.com>
2023-09-27 15:30:20 +0300

    Update headers

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,8 +8,7 @@ function Header() {
   <h2 className="text-xl font-semibold mb-2">Label issues for new Contributors</h2>
   <p className="text-gray-600">
     Now, Github will help potential first-time contributors{' '}
-    <a href="https://github.com/mumo-esther/Js-best-practices/contribute" className="text-blue-500 hover:underline">
-      discover issues </a>
+      discover issues
        labelled with
       <button className="bg-blue-500 hover:bg-blue-600 text-white px-1 py-1 ml-2 rounded-md">
         good first issue
--- src/app/components/IssueHeader.tsx
+++ src/app/components/IssueHeader.tsx
@@ -1,43 +1,43 @@
+import React, { useState, useEffect } from 'react';
 import { fetchClosedIssues, fetchOpenIssues } from "../api/githubAPI";
-import { useFilterContext } from "../context/FilterContext";
 import Selects from "./Selects";
-import React, { useState, useEffect } from "react";
 
 interface Issue {
-  state: "open" | "closed"; // Add other properties as needed
+  state: "open" | "closed"; 
 }
 
 function IssueHeader() {
   const [openIssuesCount, setOpenIssuesCount] = useState<number>(0);
   const [closedIssuesCount, setClosedIssuesCount] = useState<number>(0);
   const [selectedLabel, setSelectedLabel] = useState<string | null>(null);
-  const [selectedMilestone, setSelectedMilestone] = useState<string | null>(null);
   const [selectedAssignee, setSelectedAssignee] = useState<string | null>(null);
-
+  const [selectedMilestone, setSelectedMilestone] = useState<string | null>(null);
 
   useEffect(() => {
-    // Fetch open issues
-    fetchOpenIssues()
+    fetchOpenIssues(selectedLabel, selectedAssignee, selectedMilestone)
       .then((data: Issue[]) => {
         const openCount = data.filter((issue) => issue.state === "open").length;
         setOpenIssuesCount(openCount);
       })
       .catch((error: Error) => {
+      
         console.error("Error fetching open issues:", error);
       });
-
-    // Fetch closed issues
-    fetchClosedIssues()
+  
+    fetchClosedIssues(selectedLabel, selectedAssignee, selectedMilestone)
       .then((data: Issue[]) => {
         const closedCount = data.filter((issue) => issue.state === "closed").length;
         setClosedIssuesCount(closedCount);
       })
       .catch((error: Error) => {
+       
         console.error("Error fetching closed issues:", error);
       });
-  }, []);
+  }, [selectedLabel, selectedAssignee, selectedMilestone]);
 
+ 
   const handleFilterChange = (label: string | null, assignee: string | null, milestone: string | null) => {
+   
     setSelectedLabel(label);
     setSelectedAssignee(assignee);
     setSelectedMilestone(milestone);
@@ -46,19 +46,19 @@ function IssueHeader() {
   return (
     <div className="flex flex-col lg:flex-row justify-between items-center p-4 bg-gray-200">
       <div className="flex flex-wrap items-center space-y-2 lg:space-y-0 lg:space-x-4 mb-2 lg:mb-0">
-        <input
-          type="checkbox"
-          className="form-checkbox text-blue-500 mr-2 lg:mr-0"
-        />
-
-        <div className="w-5 h-5 border border-black rounded-full flex items-center justify-center mb-4">
-          <div className="w-1 h-1 bg-black rounded-full"></div>
-        </div>
+        
+        <label className="inline-flex items-center">
+          <input type="checkbox" className="form-checkbox text-blue-500 mr-2 lg:mr-0" />
+          <div className="w-5 h-5 border border-black rounded-full flex items-center justify-center ml-5">
+            <div className="w-1 h-1 bg-black rounded-full"></div>
+          </div>
+        </label>
         <span className="text-gray-700 mr-4 lg:mr-0">Open: {openIssuesCount}</span>
         <span className="text-gray-700">Closed: {closedIssuesCount}</span>
       </div>
       <div className="flex flex-wrap items-center space-y-2 lg:space-y-0 lg:space-x-4">
-      <Selects onFilterChange={handleFilterChange} />
+        
+        <Selects onFilterChange={handleFilterChange} />
       </div>
     </div>
   );
